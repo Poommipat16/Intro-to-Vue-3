d4b849cd5916cabbc94fae9f95e6a65852d9a3f4
Poommipat_ps <poommipat.ps@gmail.com>
2024-05-16 18:14:53 +0700

    Finished

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-const product = 'Socks'
+// Create vue app
+const app = Vue.createApp({
+    data(){
+        return{
+            product : 'Socks',
+            description : 'Hi, I am socks'
+        }
+    }
+})
